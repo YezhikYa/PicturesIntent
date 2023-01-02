8ab1d81da0cfcd4ef630a12f6dd3f32b8ec90316
student <student@A26-14>
2023-01-02 11:56:34 +0200

    Add project files.

---
+++ AvodatKita/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace AvodatKita
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private int number = 0;
+        private ImageView ivAnimal;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            InitializaViews();
+        }
+
+        private void InitializaViews()
+        {
+            ivAnimal = FindViewById<ImageView>(Resource.Id.ivAnimal);
+
+            ivAnimal.Click += ivAnimal_click;
+        }
+
+        private void ivAnimal_click(object sender, EventArgs e)
+        {
+            Intent intentRandom = new Intent(this, typeof(MainActivityRandom));
+            StartActivityForResult(intentRandom, 1);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (requestCode == 1)
+            {
+                number = data.GetIntExtra("NUMBER", 0);
+            }
+
+            switch (number)
+            {
+                case 1:
+                    ivAnimal.SetImageResource(Resource.Drawable.Bunny);
+                    break;
+                case 2:
+                    ivAnimal.SetImageResource(Resource.Drawable.Coala);
+                    break;
+                case 3:
+                    ivAnimal.SetImageResource(Resource.Drawable.Deer);
+                    break;
+                case 4:
+                    ivAnimal.SetImageResource(Resource.Drawable.Dolphin);
+                    break;
+                case 5:
+                    ivAnimal.SetImageResource(Resource.Drawable.Fox);
+                    break;
+                case 6:
+                    ivAnimal.SetImageResource(Resource.Drawable.Girafe);
+                    break;
+                case 7:
+                    ivAnimal.SetImageResource(Resource.Drawable.Hedgehog);
+                    break;
+                case 8:
+                    ivAnimal.SetImageResource(Resource.Drawable.Lemur);
+                    break;
+                case 9:
+                    ivAnimal.SetImageResource(Resource.Drawable.Sheep);
+                    break;
+                case 10:
+                    ivAnimal.SetImageResource(Resource.Drawable.Tiger);
+                    break;
+            }
+        }
+    }
+}
---
+++ AvodatKita/MainActivityRandom.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+
+namespace AvodatKita
+{
+    [Activity(Label = "MainActivityRandom")]
+    public class MainActivityRandom : Activity
+    {
+        private TextView tvNumber;
+        private Button btnGenRan;
+        private Button btnOk;
+
+        private int number;
+        private bool btnGenRanPressed = false;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_mainRandom);
+            // Create your application here
+
+            InitializeViews();
+        }
+
+        private void InitializeViews()
+        {
+            tvNumber = FindViewById<TextView>(Resource.Id.tvNumber);
+            btnGenRan = FindViewById<Button>(Resource.Id.btnGenRan);
+            btnOk = FindViewById<Button>(Resource.Id.btnOk);
+
+            number = Intent.GetIntExtra("NUMBER", 1);
+
+            btnGenRan.Click += btnGenRan_click;
+            btnOk.Click += btnOk_click;
+        }
+
+        private void btnOk_click(object sender, EventArgs e)
+        {
+            if (btnGenRanPressed)
+            {
+                Intent intent = new Intent();
+                intent.PutExtra("NUMBER", number);
+                SetResult(Result.Ok, intent);
+                Finish();
+            }
+            else
+            {
+                Toast.MakeText(Application.Context, "You haven't generated a number", ToastLength.Long).Show();
+            }
+        }
+
+        private void btnGenRan_click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            number = rnd.Next(1, 11);
+            tvNumber.Text = number.ToString();
+
+            if (number >= 1 && number <= 10)
+                btnGenRanPressed = true;
+        }
+    }
+}
